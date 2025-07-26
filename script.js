07268c5f398adffd68052b5b58189107fabaa737
ToluCodeGlitch <africanhorrorstory2@gmail.com>
2025-07-26 11:26:11 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+async function generateHash() {
+  const input = document.getElementById("inputText").value;
+  const encoder = new TextEncoder();
+  const data = encoder.encode(input);
+  const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+  document.getElementById("hashOutput").value = hashHex;
+}
